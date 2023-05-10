0f29b52574d5e293e47254c96c77438494de5a95
Sumitjh26997 <sumit.hotchandani@gmail.com>
2023-05-10 15:56:35 +0200

    feat: add private key field and message sign and key recovery functionality

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,23 +1,60 @@
 import { useState } from "react";
 import server from "./server";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { utf8ToBytes, toHex } from "ethereum-cryptography/utils";
+import { secp256k1 } from "ethereum-cryptography/secp256k1";
+
 
 function Transfer({ address, setBalance }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
+	const [privateKey, setPrivateKey] = useState("");
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
+
+	function hashMessage(message) {
+    return keccak256(utf8ToBytes(message));
+	}
+
+	async function signMessage(msg) {
+    const message = hashMessage(msg);
+    return secp256k1.sign(message, privateKey);
+	}
+
+	function getAddress(publicKey) {
+		const pubKey = publicKey.slice(1, publicKey.length);
+		const hashedKey = keccak256(pubKey);
+		return hashedKey.slice(-20);
+	}
+
+	async function verifyKey(message, signature) {
+		const hashedMessage = hashMessage(JSON.stringify(message));
+		const pubKey = signature.recoverPublicKey(hashedMessage);
+		return `0x${toHex(getAddress(pubKey.toRawBytes()))}` === address;
+	}
 
   async function transfer(evt) {
     evt.preventDefault();
 
     try {
+			const payload = {
+				sender: address,
+				amount: parseInt(sendAmount),
+				recipient,
+			};
+
+			const signature = await signMessage(JSON.stringify(payload));
+
+			if(! await verifyKey(payload, signature)) {
+				alert("You are not the owner of the sender address");
+				throw new Error("You are not the owner of the sender address");
+			}
+
+			payload.signature = signature;
+
       const {
         data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
+      } = await server.post(`send`, payload);
       setBalance(balance);
     } catch (ex) {
       alert(ex.response.data.message);
@@ -46,7 +83,16 @@ function Transfer({ address, setBalance }) {
         ></input>
       </label>
 
-      <input type="submit" className="button" value="Transfer" />
+			<label>
+        Private Key
+        <input
+          placeholder="This is only to sign the transaction, the key will not be stored"
+          value={privateKey}
+          onChange={setValue(setPrivateKey)}
+        ></input>
+      </label>
+
+      <input type="submit" className="button" value="Sign & Transfer" />
     </form>
   );
 }
